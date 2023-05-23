f8c835306dc753bbe121eac4171c0767183d5dd9
shashwat chaturvedi <shashwatchaturvedig@gmail.com>
2023-05-23 21:55:03 +0530

    introduction to hashing

---
+++ src/introduction.java
@@ -0,0 +1,21 @@
+public class introduction {
+    public static void main(String[] args) {
+        System.out.println("what is hashing ");
+
+        // Hashing is a method of sorting and indexing data > the idea behind hashing is to allow lalrge amounts of data
+        // to be indexed using keys and commonly created by formula
+
+
+        System.out.println("WHY HASHING ??");
+
+        /*
+     It is time efficient in case of search operations
+     performas better than oother data structure and algorithm in terms of searching operations
+     TIME COMPLEXITY
+     Array -o(logN)
+     Linked list - O(N)
+     Tree- O(logN )
+     hashing - O(1)
+         */
+    }
+}
